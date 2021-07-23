9d7128af0d03d383087cc26cc25bb8e20494b367
Dariioo1 <dariopaucar@outlook.es>
2021-07-23 16:21:23 +0200

    build:Initial commit

---
+++ professional.ts
@@ -0,0 +1,157 @@
+export class Professional
+{
+    public name:string;
+    public age:number;
+    public genre:string;
+    public weight:number;
+    public height:number;
+    public hairColor:string;
+    public eyeColor:string;
+    public race:string;
+    public isRetired:boolean;
+    public nationality:string;
+    public oscarsNumber:number;
+    public profession:string[]
+    public bibliografia:string
+
+    constructor(name:string, age:number, genre:string, weight:number, height:number, hairColor:string, eyeColor:string, race:string, isRetired:boolean, nationality:string, oscarsNumber:number, profession:string[], bibliografia:string )
+    {
+        this.name=name;
+        this.age=age;
+        this.genre=genre;
+        this.weight=weight;
+        this.height=height;
+        this.hairColor=hairColor;
+        this.eyeColor=eyeColor;
+        this.race=race;
+        this.isRetired=isRetired;
+        this.nationality=nationality;
+        this.oscarsNumber=oscarsNumber;
+        this.profession=profession;
+        this.bibliografia=bibliografia;
+    }
+    public getName():string
+    {
+        return this.name;
+    }
+    public getAge():number{
+        return this.age;
+    }
+    public getGenre():string
+    {
+        return this.genre
+    }
+    public getWeight():number
+    {
+        return this.weight;
+    }
+    public getHeight():number
+    {
+        return this.height
+    }
+    public getHairColor():string
+    {
+        return this.hairColor;
+    }
+    public getEyeColor():string
+    {
+        return this.eyeColor;
+    }
+    public getRace():string
+    {
+        return this.race;
+    }
+    public getIsRetired():boolean
+    {
+        return this.isRetired;
+    }
+    public getNationality():string
+    {
+        return this.nationality;
+    }
+    public getOscarsNumber():number
+    {
+        return this.oscarsNumber;
+    }
+    public getProfession():string[]
+    {
+        return this.profession;
+    }
+    public getBibliografia():string
+    {
+        return this.bibliografia;
+    }
+    public setName(name:string)
+    {
+        this.name=name;
+
+    }
+    public setAge(age:number)
+    {
+        this.age=age;
+    }
+    public setGenre(genre:string)
+    {
+        this.genre=genre;
+    }
+    public setWeight(weight:number)
+    {
+        this.weight=weight;
+    }
+    public setHeight(height:number)
+    {
+        this.height=height;
+    }
+    public setHairColor(hairColor:string)
+    {
+        this.hairColor=hairColor;
+    }
+    public setEyeColor(eyeColor:string)
+    {
+        this.eyeColor=eyeColor;
+    }
+    public setRace(race:string)
+    {
+        this.race=race;
+    }
+    public serIsRetired(isRetired:boolean)
+    {
+        this.isRetired=isRetired;
+    }
+    public setNationality(nationality:string)
+    {
+        this.nationality=nationality;
+    }
+    public setOscarsNumber(oscarsNumber:number)
+    {
+        this.oscarsNumber=oscarsNumber;
+    }
+    public setProfession(profession:string[])
+    {
+        this.profession=profession;
+    }
+    public setBibliografia(bibliografia:string)
+    {
+        this.bibliografia=bibliografia;
+    }
+
+    public printProfessional()
+    {
+        console.log("This characteristics of Professional " + this.name + " are");
+        console.log("-Name: " + this.name);
+        console.log("-Age: " + this.age);
+        console.log("-Genre: " + this.genre);
+        console.log("-Weight: " + this.weight);
+        console.log("-Height: " + this.height);
+        console.log("-Hair Color: " + this.hairColor);
+        console.log("-Eye Color: " + this.eyeColor);
+        console.log("-Race: " + this.race);
+        console.log("-Is Retired ?: " + this.isRetired);
+        console.log("-Nationality: " + this.nationality);
+        console.log("-Oscars Number: " + this.oscarsNumber);
+        console.log("-Profession: " + this.profession );
+        console.log("-Bibliografia Completo:" + this.bibliografia + "\n");
+        
+    }
+
+}
