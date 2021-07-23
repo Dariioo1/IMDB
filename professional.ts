332347e8768389d705e9eff613035c92a88de160
fpicong71 <fpicong@yahoo.es>
2021-07-23 20:16:37 +0200

    style(professional.ts professionalTest.ts) : Cambios de estilo. indentaciones y comentarios

--- professional.ts
+++ professional.ts
@@ -1,5 +1,6 @@
 export class Professional
 {
+    //ATRIBUTOS
     public name:string;
     public age:number;
     public genre:string;
@@ -12,9 +13,24 @@ export class Professional
     public nationality:string;
     public oscarsNumber:number;
     public profession:string[]
-    public bibliografia:string
+    public bio:string
 
-    constructor(name:string, age:number, genre:string, weight:number, height:number, hairColor:string, eyeColor:string, race:string, isRetired:boolean, nationality:string, oscarsNumber:number, profession:string[], bibliografia:string )
+    //CONSTRUCTOR
+
+    constructor(
+        name:string, 
+        age:number,
+        genre:string, 
+        weight:number, 
+        height:number, 
+        hairColor:string, 
+        eyeColor:string, 
+        race:string, 
+        isRetired:boolean, 
+        nationality:string, 
+        oscarsNumber:number, 
+        profession:string[], 
+        bio:string )
     {
         this.name=name;
         this.age=age;
@@ -28,116 +44,144 @@ export class Professional
         this.nationality=nationality;
         this.oscarsNumber=oscarsNumber;
         this.profession=profession;
-        this.bibliografia=bibliografia;
+        this.bio=bio;
     }
+
+    //GETTERS & SETTERS
+    //GEt---------------
     public getName():string
     {
         return this.name;
     }
+
     public getAge():number{
         return this.age;
     }
+
     public getGenre():string
     {
         return this.genre
     }
+
     public getWeight():number
     {
         return this.weight;
     }
+
     public getHeight():number
     {
         return this.height
     }
+
     public getHairColor():string
     {
         return this.hairColor;
     }
+
     public getEyeColor():string
     {
         return this.eyeColor;
     }
+
     public getRace():string
     {
         return this.race;
     }
+
     public getIsRetired():boolean
     {
         return this.isRetired;
     }
+
     public getNationality():string
     {
         return this.nationality;
     }
+
     public getOscarsNumber():number
     {
         return this.oscarsNumber;
     }
+
     public getProfession():string[]
     {
         return this.profession;
     }
-    public getBibliografia():string
+
+    public getBio():string
     {
-        return this.bibliografia;
+        return this.bio;
     }
+
+    //Set----------------------
     public setName(name:string)
     {
         this.name=name;
-
     }
+
     public setAge(age:number)
     {
         this.age=age;
     }
+
     public setGenre(genre:string)
     {
         this.genre=genre;
     }
+
     public setWeight(weight:number)
     {
         this.weight=weight;
     }
+
     public setHeight(height:number)
     {
         this.height=height;
     }
+
     public setHairColor(hairColor:string)
     {
         this.hairColor=hairColor;
     }
+
     public setEyeColor(eyeColor:string)
     {
         this.eyeColor=eyeColor;
     }
+
     public setRace(race:string)
     {
         this.race=race;
     }
+
     public serIsRetired(isRetired:boolean)
     {
         this.isRetired=isRetired;
     }
+
     public setNationality(nationality:string)
     {
         this.nationality=nationality;
     }
+
     public setOscarsNumber(oscarsNumber:number)
     {
         this.oscarsNumber=oscarsNumber;
     }
+
     public setProfession(profession:string[])
     {
         this.profession=profession;
     }
-    public setBibliografia(bibliografia:string)
+
+    public setBio(bio:string)
     {
-        this.bibliografia=bibliografia;
+        this.bio=bio;
     }
 
     public printProfessional()
     {
-        console.log("This characteristics of Professional " + this.name + " are");
+        console.log("The characteristics of Professional " + this.name + " are");
         console.log("-Name: " + this.name);
         console.log("-Age: " + this.age);
         console.log("-Genre: " + this.genre);
@@ -150,8 +194,7 @@ export class Professional
         console.log("-Nationality: " + this.nationality);
         console.log("-Oscars Number: " + this.oscarsNumber);
         console.log("-Profession: " + this.profession );
-        console.log("-Bibliografia Completo:" + this.bibliografia + "\n");
-        
+        console.log("-Biografia Completa:" + this.bio + "\n");        
     }
 
 }
--- professionalTest.ts
+++ professionalTest.ts
@@ -1,15 +1,131 @@
+// /Importamos la clase Professional
 import { Professional } from './professional';
 
-let profesional1:Professional = new Professional ("Rose Bryne", 41 , "Mujer" , 53 , 168, "Castaño Oscuro" , "Marron Cafe", "Blanca" , false , "Australiana" , 0 , ["Actor"], " https://en.wikipedia.org/wiki/Rose_Byrne");
-let profesional2:Professional = new Professional ("Sylvester Stallone" , 75 , "Hombre" , 102 , 177 , "Marron Oscuro" , "Cafe" , "Blanca" , false, "Estadounidense", 3 , ["Actor " , "Productor " , "Guinonista ", " Director"], " https://en.wikipedia.org/wiki/Sylvester_Stallone");
-let profesional3:Professional = new Professional ("Vivien Leigh", 53 , "Mujer" , 55 , 160 , "Castaño" , "Azules", "Blanca", true , "Britanica", 2 , ["Actriz"] , " https://en.wikipedia.org/wiki/Vivien_Leigh");
-let profesional4:Professional = new Professional ("Russell Crowe" , 57 , "Hombre" , 92 , 182 , "Marron Claro", "Azules", "Blanca", false, "Neozelandes", 1 , ["Actor"] , " https://en.wikipedia.org/wiki/Russell_Crowe");
-let profesional5:Professional = new Professional ("Robert John Downey" , 51 , "Hombre" , 78 , 174 , "Marron Oscuro" , "Marron Oscuro" , "Blanco" , false , "Estadounidense" , 2 , ["Actor " , " Compositor" , " Director"] , " https://ast.wikipedia.org/wiki/Robert_Downey_Jr.")
-let Profesional6:Professional = new Professional ("Anthony Hopkins" , 83 , "Hombre" , 70 , 174 , "Gris" , "Azules" , "Blanca" , false , "Estadounidense" , 2 , ["Actor"], " https://en.wikipedia.org/wiki/Anthony_Hopkins") 
 
+// Definimos varios objetos de la clase professional
+
+let profesional1:Professional = 
+                new Professional (
+                    "Rose Bryne", 
+                    41 , 
+                    "Mujer" , 
+                    53 , 
+                    168, 
+                    "Castaño Oscuro" , 
+                    "Marron Cafe", 
+                    "Blanca" , 
+                    false , 
+                    "Australiana" , 
+                    0 , 
+                    ["Actor"], 
+                    " https://en.wikipedia.org/wiki/Rose_Byrne"
+                );
+
+
+let profesional2:Professional =
+                new Professional (
+                    "Sylvester Stallone" , 
+                    75 , 
+                    "Hombre" , 
+                    102 , 
+                    177 , 
+                    "Marron Oscuro" , 
+                    "Cafe" , 
+                    "Blanca" , 
+                    false, 
+                    "Estadounidense", 
+                    3 , 
+                    ["Actor " , "Productor " , "Guinonista ", " Director"], 
+                    " https://en.wikipedia.org/wiki/Sylvester_Stallone"
+                    );
+
+
+let profesional3:Professional = 
+                    new Professional (
+                        "Vivien Leigh", 
+                        53 , 
+                        "Mujer" , 
+                        55 , 
+                        160 , 
+                        "Castaño" , 
+                        "Azules", 
+                        "Blanca", 
+                        true , 
+                        "Britanica", 
+                        2 , 
+                        ["Actriz"] , 
+                        " https://en.wikipedia.org/wiki/Vivien_Leigh"
+                        );
+
+
+let profesional4:Professional = 
+                    new Professional (
+                        "Russell Crowe" , 
+                        57 , 
+                        "Hombre" , 
+                        92 , 
+                        182 , 
+                        "Marron Claro", 
+                        "Azules", 
+                        "Blanca", 
+                        false, 
+                        "Neozelandes", 
+                        1 , 
+                        ["Actor"] , 
+                        " https://en.wikipedia.org/wiki/Russell_Crowe"
+                        );
+
+
+let profesional5:Professional = 
+                    new Professional (
+                        "Robert John Downey" , 
+                        51 , 
+                        "Hombre" , 
+                        78 , 
+                        174 , 
+                        "Marron Oscuro" , 
+                        "Marron Oscuro" , 
+                        "Blanco" , 
+                        false , 
+                        "Estadounidense" , 
+                        2 , 
+                        ["Actor " , " Compositor" , " Director"] , 
+                        " https://ast.wikipedia.org/wiki/Robert_Downey_Jr."
+                        );
+
+
+let Profesional6:Professional = 
+                    new Professional (
+                        "Anthony Hopkins" , 
+                        83 , 
+                        "Hombre" , 
+                        70 , 
+                        174 , 
+                        "Gris" , 
+                        "Azules" , 
+                        "Blanca" , 
+                        false , 
+                        "Estadounidense" , 
+                        2 , 
+                        ["Actor"], 
+                        " https://en.wikipedia.org/wiki/Anthony_Hopkins"
+                        ); 
+
+
+//Probamos el método printProfessional                        
 profesional1.printProfessional();
+console.log("\n--------------------------\n");
+
 profesional2.printProfessional();
+console.log("\n--------------------------\n");
+
 profesional3.printProfessional();
+console.log("\n--------------------------\n");
+
 profesional4.printProfessional();
+console.log("\n--------------------------\n");
+
 profesional5.printProfessional();
+console.log("\n--------------------------\n");
+
 Profesional6.printProfessional();
