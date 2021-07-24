f4cea7ffe113cc28a28e011b76cfc9c140e8b065
Dariioo1 <dariopaucar@outlook.es>
2021-07-24 18:55:14 +0200

    feature(professionalTest.ts):MetodosNuevos

--- professionalTest.ts
+++ professionalTest.ts
@@ -110,8 +110,136 @@ let Profesional6:Professional =
                         ["Actor"], 
                         " https://en.wikipedia.org/wiki/Anthony_Hopkins"
                         ); 
+                        let professional7:Professional = 
+                    new Professional(
+                        "John G.Advilsen" , 
+                        81 , 
+                        "Hombre" , 
+                        70 , 
+                        165 , 
+                        "Gris" , 
+                        "Negro", 
+                        "Blanca" , 
+                        true , 
+                        "Estadounidense" , 
+                        1 , 
+                        ["Director"] , 
+                        "https://en.wikipedia.org/wiki/John_G._Avildsen"
+                        );
+let professional8:Professional =
+                    new Professional(
+                        "Robert Chartoff" , 
+                        87 , 
+                        "Hombre" , 
+                        80 , 
+                        176 , 
+                        "Negro" , 
+                        "Negro" , 
+                        "Blanca" , 
+                        true , 
+                        "Estadounidense" , 
+                        3 , 
+                        ["Productor"] , 
+                        "https://en.wikipedia.org/wiki/Robert_Chartoff"
+                        );
+let professional9:Professional =
+                    new Professional(
+                        "Joss Whedon " , 
+                        57 , 
+                        "Hombre" , 
+                        80 , 
+                        178 , 
+                        "Rojo" , 
+                        "Gris" , 
+                        "Blanca" , 
+                        false , 
+                        "Estadounidense" , 
+                        1 , 
+                        ["Director"] , 
+                        "https://en.wikipedia.org/wiki/Joss_Whedon" 
+                        );
+let professional10:Professional =
+                    new Professional(
+                        "Kevin Feige" , 
+                        48 , 
+                        "Hombre" , 
+                        80 , 
+                        182 , 
+                        "Marron Oscuro" , 
+                        "Marron" , 
+                        "Blanca" , 
+                        false , 
+                        "Estadounidense" , 
+                        1 , 
+                        ["Productor"] , 
+                        "https://en.wikipedia.org/wiki/Kevin_Feige"
+                        );
+let professional11:Professional =
+                    new Professional(
+                        "Stan Lee" , 
+                        96 , 
+                        "Hombre" , 
+                        64 , 
+                        174 , 
+                        "Gris" , 
+                        "Azules" , 
+                        "Blanca" , 
+                        true , 
+                        "Estadounidense" , 
+                        1 , 
+                        ["Escritor"] , 
+                        "https://en.wikipedia.org/wiki/Stan_Lee"
+                        );
+let professional12:Professional =
+                    new Professional(
+                        "Jonathan Demme" , 
+                        77 , 
+                        "Hombre" , 
+                        75 , 
+                        177 , 
+                        "Negro" , 
+                        "Marron" , 
+                        "Blanca" , 
+                        false , 
+                        "Estadounidense" , 
+                        1 , 
+                        ["Director"] , 
+                        "https://en.wikipedia.org/wiki/Jonathan_Demme"
+                        );
+let professional13:Professional =
+                    new Professional(
+                        "Ted Dally" , 
+                        69 , 
+                        "Hombre" , 
+                        86 , 
+                        174 , 
+                        "Marron Oscuro" , 
+                        "Negro" , 
+                        "Blanca" , 
+                        false , 
+                        "Estadounidense" , 
+                        1 , 
+                        ["Escritor"] , 
+                        "https://en.wikipedia.org/wiki/Ted_Tally"
+                        );
+let professional14:Professional =
+                    new Professional(
+                        "Edward Saxon" , 
+                        87 ,
+                        "Hombre" , 
+                        80 , 
+                        176 , 
+                        "Negro" , 
+                        "Negro" , 
+                        "Blanca" , 
+                        true  , 
+                        "Estadounidense" , 
+                        3 , 
+                        ["Productor"] , 
+                        "https://en.wikipedia.org/wiki/Robert_Chartoff"
+                        );
 
-
+                        
 //Probamos el método printProfessional                        
 profesional1.printProfessional();
 console.log("\n--------------------------\n");
@@ -129,3 +257,32 @@ profesional5.printProfessional();
 console.log("\n--------------------------\n");
 
 Profesional6.printProfessional();
+console.log("\n--------------------------\n");
+
+professional7.printProfessional();
+console.log("\n--------------------------\n");
+
+professional8.printProfessional();
+console.log("\n--------------------------\n");
+
+professional9.printProfessional();
+console.log("\n--------------------------\n");
+
+professional10.printProfessional();
+console.log("\n--------------------------\n");
+
+professional11.printProfessional();
+console.log("\n--------------------------\n");
+
+professional12.printProfessional();
+console.log("\n--------------------------\n");
+
+professional13.printProfessional();
+console.log("\n--------------------------\n");
+
+professional14.printProfessional();
+
+
+
+
+
